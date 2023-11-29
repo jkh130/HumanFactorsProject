a58178e6ca6f9d548176a9ab60b74ec73b27b3c1
Stanley Nwajiaku <csn30@txstate.edu>
2023-11-29 14:40:34 -0600

    added comment option for live updates

--- restaurant-app/server.js
+++ restaurant-app/server.js
@@ -27,6 +27,60 @@ app.get('/api/info/:restaurantName', (req, res) => {
   });
 });
 
+// New endpoint to post a comment for a specific restaurant
+app.post('/api/comments/:restaurantId', async (req, res) => {
+  const restaurantId = req.params.restaurantId;
+  const commentData = {
+    ...req.body,
+    restaurant_id: restaurantId,
+    timestamp: new Date().toISOString()
+  };
+  const filePath = path.join(DATABASE_PATH, 'comments.json');
+
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    let comments = data ? JSON.parse(data) : [];
+    comments.push(commentData);
+    await fs.promises.writeFile(filePath, JSON.stringify(comments, null, 2));
+    res.status(201).json(comments); // Send back the updated comments array
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      // If the file doesn't exist, create it with the new comment
+      await fs.promises.writeFile(filePath, JSON.stringify([commentData], null, 2));
+      res.status(201).json([commentData]); // Send back the array with the new comment
+    } else {
+      res.status(500).send('Server error');
+    }
+  }
+});
+
+
+// New endpoint to get comments for a specific restaurant
+app.get('/api/comments/:restaurantId', (req, res) => {
+  const restaurantId = req.params.restaurantId;
+  const filePath = path.join(DATABASE_PATH, 'comments.json');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err && err.code === 'ENOENT') {
+      // If the file doesn't exist, send back an empty array
+      res.json([]);
+    } else if (err) {
+      res.status(500).send('Server error');
+    } else {
+      let comments = [];
+      try {
+        comments = JSON.parse(data);
+      } catch (parseErr) {
+        console.error('Error parsing JSON:', parseErr);
+        res.status(500).send('Error parsing comments data');
+        return;
+      }
+      const restaurantComments = comments.filter(comment => comment.restaurant_id === restaurantId);
+      res.json(restaurantComments);
+    }
+  });
+});
+
 // Catch-all handler for serving index.html must be declared after all other API routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
